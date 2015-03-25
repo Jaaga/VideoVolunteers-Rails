a38a76402a40d4861280436c546f4f5612768ea7
Brice <bgatelet@gmail.com>
2015-03-25 21:05:06 +0530

    Finish impact system

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,14 +1,33 @@
 class TrackersController < ApplicationController
 
   def index
-    @trackers = Tracker.all.order("uid DESC")
+    if params[:recent]
+      @trackers = Tracker.where(updated_at: Date.today-14...Date.today+1).order("updated_at DESC")
+    elsif !params[:state_name].blank?
+      @state = State.find_by(name: params[:state_name])
+      @trackers = @state.trackers.order("uid DESC")
+    else
+      @trackers = Tracker.all
+    end
+
     @columns = Tracker.column_names - ['id', 'tracker_details_id',
-                                       'tracker_details_type']
+                                         'tracker_details_type']
   end
 
   def show
     @tracker = Tracker.find(params[:id])
     @columns = view_context.array_set
+
+    if @tracker.uid.include?('_impact') && !@tracker.original_uid.blank?
+      @columns.except!(:impact_planning, :impact_achieved, :impact_video)
+    end
+
+    if @tracker.uid.include?('_impact')
+      @columns[:extra] -= ['impact_uid']
+    else
+      @columns[:extra] -= ['original_uid', 'no_original_uid']
+    end
+
     @sections = @columns.keys
     @sections -= [:special, :yesno, :numbers]
   end
@@ -16,6 +35,8 @@ class TrackersController < ApplicationController
   def new
     @tracker = Tracker.new
     @state = State.find_by(name: params[:state_name])
+    # For dropdown of issue videos that have no impact videos linked to them.
+    @state_videos = @state.trackers.where("impact_uid IS NULL AND uid NOT LIKE '%_impact'")
     @columns = view_context.array_set
     @unique = view_context.unique_set
     @context = "new"
@@ -24,16 +45,28 @@ class TrackersController < ApplicationController
   def create
     @tracker = Tracker.new(tracker_params)
 
-    if params[:tracker][:is_impact] != "0"
+    if params[:tracker][:is_impact] == "1"
       if params[:tracker][:original_uid].blank? && params[:tracker][:no_original_uid].blank?
         flash.now[:error] = "Need an original_uid or a reason for not having one if
                          this is an impact video"
+        failed_form_create
+        return
+      elsif !params[:tracker][:original_uid].blank? && !params[:tracker][:no_original_uid].blank?
+        flash.now[:error] = "You don't need a reason for not having an original
+                             uid if you have selected an original uid."
         failed_form_create
         return
       end
       # This variable will be passed on to the UID setter so that impact UID's
       # can be made.
       @is_impact = true
+    else
+      unless params[:tracker][:original_uid].blank? && params[:tracker][:no_original_uid].blank?
+        flash.now[:error] = "The 'is impact' checkbox needs to be checked if it
+                             is an impact video."
+        failed_form_create
+        return
+      end
     end
 
     unless params[:tracker][:cc_name].blank?
@@ -57,6 +90,11 @@ class TrackersController < ApplicationController
   def edit
     @tracker = Tracker.find(params[:id])
     @columns = view_context.array_set
+
+    if @tracker.uid.include?('_impact') && !@tracker.original_uid.blank?
+      @columns.except!(:impact_planning, :impact_achieved, :impact_video)
+    end
+
     @sections = @columns.keys
     @sections -= [:extra]
     @unique = view_context.unique_set
