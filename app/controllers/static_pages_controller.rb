bd21f7712b1e866dd7f6c3e99f614593164ca50b
Brice <bgatelet@gmail.com>
2015-03-31 18:44:12 +0530

    Finish stats page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,56 +11,67 @@ class StaticPagesController < ApplicationController
       end
     end
 
-    @stats = Array.new(24) { Array.new }
+    @stats = Array.new(25) { Array.new }
     @ccs_pitch = Array.new
     @active_states.each do |state|
       # No. of story ideas pitched
-      @stats[0].push(state.trackers.where("story_pitch_date IS NOT NULL AND backup_received_date IS NULL").count)
+      @stats[0].push(state.trackers.where("story_pitch_date IS NOT NULL").count)
       # No. of CCs who pitched story ideas
       @stats[1].push(state.ccs.where("last_pitched_story_idea_date IS NOT NULL").count)
       # No. of CCs who made issue videos
       @stats[2].push(state.ccs.where("last_issue_video_made_date IS NOT NULL").count)
       # No. of inactive CCs who made issue videos this quarter
-      @stats[3].push(state.trackers.where("story_pitch_date IS NOT NULL AND backup_received_date IS NULL").count)
+      @stats[3].push(state.ccs.where("is_inactive = ? AND last_issue_video_made_date > ? AND last_issue_video_made_date > ?", 'true', Time.now.months_ago(3).to_date, Time.now.to_date).count)
 
       # No. of story ideas pitched so far this calendar month
       @stats[4].push(state.trackers.where("story_pitch_date > ? AND story_pitch_date < ?", Time.now.beginning_of_month.to_date, Time.now.end_of_month.to_date).count)
       # no of story ideas pitched in the last 3 months
       @stats[5].push(state.trackers.where("story_pitch_date > ? AND story_pitch_date < ?", Time.now.months_ago(3).to_date, Time.now.to_date).count)
       # no of CC's who pitched a story this month
+      @stats[6].push(state.ccs.where("last_pitched_story_idea_date > ? AND last_pitched_story_idea_date < ?", Time.now.beginning_of_month.to_date, Time.now.end_of_month.to_date).count)
       # no of CC's who pitched a story in the last 3 months
+      @stats[7].push(state.ccs.where("last_pitched_story_idea_date > ? AND last_pitched_story_idea_date < ?", Time.now.months_ago(3).to_date, Time.now.to_date).count)
 
-      # No of issue videos made so far this month  ie raw footage received in state office
+      # No of issue videos made so far this month ie raw footage received in state office
+      @stats[8].push(state.trackers.where("raw_footage_review_date > ? AND raw_footage_review_date < ?", Time.now.beginning_of_month.to_date, Time.now.end_of_month.to_date).count)
       # no of issue videos made I the last 3 months ie raw footage received in state office
-      # no of CC's who made an issue video this monthie raw footage received in state office
+      @stats[9].push(state.trackers.where("raw_footage_review_date > ? AND raw_footage_review_date < ?", Time.now.months_ago(3).to_date, Time.now.to_date).count)
+      # no of CC's who made an issue video this month ie raw footage received in state office
+      @stats[10].push(state.ccs.where("last_issue_video_made_date > ? AND last_issue_video_made_date < ?", Time.now.beginning_of_month.to_date, Time.now.end_of_month.to_date).count)
       # no of CC's who made an issue video in the last three months ie raw footage received in state office
+      @stats[11].push(state.ccs.where("last_issue_video_made_date > ? AND last_issue_video_made_date < ?", Time.now.months_ago(3).to_date, Time.now.to_date).count)
 
-      # no of impacts achived this month
+      # no of impacts achieved this month
+      @stats[12].push(state.trackers.where("impact_date > ? AND impact_date < ?", Time.now.beginning_of_month.to_date, Time.now.end_of_month.to_date).count)
       # no of CC's who achieved an impact this month
+      @stats[13].push(state.ccs.where("last_impact_achieved_date > ? AND last_impact_achieved_date < ?", Time.now.beginning_of_month.to_date, Time.now.end_of_month.to_date).count)
       # no of impacts achieved in the last 3 months
+      @stats[14].push(state.trackers.where("impact_date > ? AND impact_date < ?", Time.now.months_ago(3).to_date, Time.now.to_date).count)
       # no of CC's who achieved an impact in the last 3 months
+      @stats[15].push(state.ccs.where("last_impact_achieved_date > ? AND last_impact_achieved_date < ?", Time.now.months_ago(3).to_date, Time.now.to_date).count)
 
       ### State
       # videos on hold
-      @stats[16].push(state.trackers.where("office_responsible = ? AND proceed_with_edit_and_payment = ?", 'State', 'On hold').count)
+      @stats[16].push(state.trackers.where("office_responsible = ? AND edit_status = ?", 'State', 'on hold').count)
       # State Edit Bank: total approved videos whose edits haven't reached goa (for states with regional editors only) (including on hold)
-      # Is this redundant?
-      @stats[17].push(state.trackers.where("edit_received_in_goa_date IS NULL AND proceed_with_edit_and_payment = ?", 'On hold').count)
-      # approved videos
-      @stats[18].push(state.trackers.where("office_responsible = ? AND proceed_with_edit_and_payment = ?", 'State', 'Cleared').count)
+      @stats[17].push(state.trackers.where("office_responsible = ? AND edit_status != ?", 'State', 'video under edit').count)
+      # approved videos (everything not on hold)
+      @stats[18].push(state.trackers.where("office_responsible = ? AND edit_status = ?", 'State', 'on hold').count)
 
       ### Goa
       # videos on hold
-      @stats[19].push(state.trackers.where("office_responsible = ? AND proceed_with_edit_and_payment = ?", 'Goa', 'On hold').count)
-      # videos that are in Goa and need to be edited, reviewed & uploaded
-      @stats[20].push(state.trackers.where("edit_received_in_goa_date IS NOT NULL AND office_responsible = ?", 'Goa').count)
-      # videos whose rough cuts have arrived from a state office and need to be cleaned, reviewed and uploaded
-      @stats[21].push(state.trackers.where("office_responsible = ? AND review_date IS NOT NULL", 'Goa').count)
+      @stats[19].push(state.trackers.where("office_responsible = ? AND edit_status = ?", 'Goa', 'on hold').count)
+      # videos to be edited
+      @stats[20].push(state.trackers.where("office_responsible = ? AND edit_status = ?", 'Goa', 'video under edit').count)
+      # videos to be reviewed
+      @stats[21].push(state.trackers.where("office_responsible = ? AND edit_status = ?", 'Goa', 'video under review').count)
+      # videos to be finalized and uploaded
+      @stats[22].push(state.trackers.where("office_responsible = ? AND edit_status = ?", 'Goa', 'video to be finalised and uploaded (i.e. after review)').count)
 
       # Total UID's
-      @stats[22].push(state.trackers.count)
+      @stats[23].push(state.trackers.count)
       # Videos uploaded to youtube
-      @stats[23].push(state.trackers.where("youtube_date IS NOT NULL").count)
+      @stats[24].push(state.trackers.where("youtube_date IS NOT NULL").count)
     end
   end
 
