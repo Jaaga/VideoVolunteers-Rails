e04cbd1e30a85e44ecb5489370df0feb4daef49c
Brice <bgatelet@gmail.com>
2015-04-02 16:12:36 +0530

    Fix redirect loop and admin for mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,12 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_in_path_for(resource)
+    home_path
+  end
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
-    root_path
+    new_user_session_path
   end
 end
